56e285b9d0e188011d2df365d3e7968651c9f18e
unknown <dockadams333@gmail.com>
2023-07-08 15:58:51 +0700

    Breadth first search algorithm

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { gameBoard } from "./gameBoard";
 import { knight } from "./knight";
 
-const start = [4, 5];
-const end = [7, 0];
+// const start = [4, 5];
+// const end = [7, 0];
+
+const start = [0, 2];
+const end = [3, 6];
 
 gameBoard.display();
 knight.movePath(start, end, gameBoard);
 
 const initialDestinations = knight.getPossibleMoves(start, gameBoard);
-knight.searchMoveTree(initialDestinations, gameBoard, end);
+// knight.randomSearch(initialDestinations, gameBoard, end);
+knight.breadthFirstSearch(start, end, gameBoard, true);
--- src/knight.js
+++ src/knight.js
@@ -1,7 +1,6 @@
 const knight = {
-  speak() {
-    console.log("I am a knight.");
-  },
+  queue: [],
+  visited: new Set(),
   display: { start: "KNS", end: "KNE", dest: "X", path: "P" },
   movements: {
     0: [-2, -1],
@@ -19,20 +18,21 @@ const knight = {
   },
   getPossibleMoves(start, board) {
     const initialDestinations = [];
-    board.clearDestinations(this.display.dest);
+    board.clearDestinations(this.display.dest, true);
+    board.placePiece(this, start, "start");
 
     for (let move in this.movements) {
       const destination = start.map(
         (elem, index) => elem + this.movements[move][index]
       );
       if (!board.outOfBounds(destination)) {
-        board.placePiece(this, destination, "dest");
+        // board.placePiece(this, destination, "dest");
         initialDestinations.push(destination);
       }
     }
     return initialDestinations;
   },
-  searchMoveTree(destinations, board, endpoint) {
+  randomSearch(destinations, board, endpoint) {
     //pass a reference to the recursive call to trace the path
     //of its endpoint
     let endPointReached = false;
@@ -55,16 +55,63 @@ const knight = {
     const knightTraverse =
       destinations[Math.floor(Math.random() * destinations.length)];
 
-    //place knight-start at new landing position
-    board.placePiece(this, knightTraverse, "start");
-
     //get new branch from landing position
     const newBranch = this.getPossibleMoves(knightTraverse, board);
 
     //recursive call
     setTimeout(() => {
-      this.searchMoveTree(newBranch, board, endpoint);
+      this.randomSearch(newBranch, board, endpoint);
     }, 500);
+  },
+  breadthFirstSearch(current, endpoint, board, initial, parentMap = {}) {
+    if (initial) {
+      this.queue.push(current);
+      this.visited.add(JSON.stringify(current));
+      parentMap[JSON.stringify(current)] = null;
+    }
+
+    while (this.queue.length > 0) {
+      const node = this.queue.shift();
+
+      if (JSON.stringify(node) === JSON.stringify(endpoint)) {
+        console.log("Found endpoint.");
+        console.log(this.queue);
+        console.log(this.visited);
+
+        // Retrace the path from the endpoint to the start
+        let path = [node];
+        let parent = parentMap[JSON.stringify(node)];
+        while (parent) {
+          path.unshift(parent);
+          parent = parentMap[JSON.stringify(parent)];
+        }
+
+        console.log("Path:", path);
+
+        // Place the knight on each element of the path
+        for (let i = 0; i < path.length; i++) {
+          const pathElement = path[i];
+          setTimeout(() => {
+            board.clearDestinations(this.display.token, true);
+            board.placePiece(this, pathElement, "start");
+          }, i * 1000);
+        }
+
+        return;
+      }
+
+      const connections = this.getPossibleMoves(node, board);
+      for (let i = 0; i < connections.length; i++) {
+        const connection = connections[i];
+        const connectionString = JSON.stringify(connection);
+
+        if (!this.visited.has(connectionString)) {
+          this.queue.push(connection);
+          this.visited.add(connectionString);
+          parentMap[connectionString] = node;
+        }
+      }
+    }
   },
 };
 
